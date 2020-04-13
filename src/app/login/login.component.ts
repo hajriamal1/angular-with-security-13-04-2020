48812a05a944b8be98f421433725cbccb71f93c5
hajriamall <hajriamal55@gmail.com>
2020-04-13 08:45:38 +0100

    quelques modification

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -55,7 +55,7 @@ export class LoginComponent implements OnInit {
                   );
                 }
                 gotohome(){
-                  this.router.navigate(['home']);
+                  this.router.navigate(['home/dashboard']);
                 }
                 reloadPage() {
                   window.location.reload();
